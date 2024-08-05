c6b886b49e057356ffdb28542a37a230ba18d02b
Beatriz da Cruz Bacelar <69614234+BeatrizBacelar@users.noreply.github.com>
2024-08-05 17:58:39 -0300

    feat: add new tokens

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,13 +1,38 @@
 /* token.h */
 
 typedef enum {
-        ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
+        ID = 256,
+        NUM, 
+        //KEY 
+        ARRAY, // 258
+        BOOLEAN, // 259
+        CHART,
+        ELSE,
+        FALSE,
+        FOR,
+        FUNCTION,
+        IF,
+        INTEGER,
+        PRINT,
+        RETURN,
+        STRING,
+        TRUE,
+        VOID,
+        WHILE,  
+        //SYMC
+        LTE, // <= 273
+        GTE, // >=
+        EQU, // ==
+        NEQ, // !=
+        AND, // &&
+        OR, // ||
+        //SYM,  
         CHAR,
-	STR,
-	ERROR
+        STR,
+        ERROR
 } token_t; 
+
+
+
+
 
